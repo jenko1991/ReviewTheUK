5d00448889b86d5401354b953d7e57d17cc93111
jenko1991 <lewisjenkin@live.co.uk>
2021-11-26 12:05:00 +0000

    Initial commit

---
+++ topjs.js
@@ -0,0 +1,14 @@
+window.onload=function(){
+    $('.container').slick({
+    autoplay:true,
+    autoplaySpeed:1500,
+    arrows:true,
+    prevArrow:'<button type="button" class="slick-prev"></button>',
+    nextArrow:'<button type="button" class="slick-next"></button>',
+    centerMode:true,
+    slidesToShow:1,
+    slidesToScroll:2
+    });
+  };
+
+
